5f299058436315440538ccc7461f9497688361c0
TouchPad <olsher258@mail.ru>
2021-01-12 22:23:54 +0300

    Наследия , переопределения метода, полиморфизм

---
+++ Tests/ConsoleTest/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Printer
+    {
+        public virtual /*virtual Дает возможность наследникам переопределить этот метод*/ void Print(string txt) => Console.WriteLine(txt);
+
+        public override string ToString() => "Принтер"; //переопределения метода тостринг
+    }
+
+    class PrefixPrinter : Printer // наследник
+    {
+        public string Prefix { get; set; }
+
+        public PrefixPrinter(string Prefix)
+        {
+            this.Prefix = Prefix;
+        }
+
+        public override /*override  можем изменить логику метода принт*/ void Print(string txt) => Console.WriteLine($"{Prefix}{txt}");
+        // public override string ToString() => "Принтер с префиксом"; //переопределения метода тостринг
+
+        public override string ToString() // вызов стандартный тостринг
+        {
+            return base.ToString() + " с Префиксом";
+        }
+    }
+}
