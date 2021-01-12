c302be68995192638485a2a16141ba07ec14fc8e
TouchPad <olsher258@mail.ru>
2021-01-12 21:53:19 +0300

    Перегрузка опереторов

---
+++ Tests/ConsoleTest/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Player
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; } = DateTime.Now; // Присвоить дату этого дня
+
+        public Player()
+        {
+
+        }
+        public Player(string Name)
+        {
+            this.Name = Name;
+        }
+        public Player(string Name, string Surname)
+            : this(Name) // вызов другой конструктор
+        {
+            this.Surname = Surname;
+        }
+    }
+}
--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
             //Vector2DClass unit =  Vector2DClass.Unit;
-            //Vector2DClass zero = Vector2DClass.Zero;  статические поля
+            //Vector2DClass zero = Vector2DClass.Zero;  обращения к статическим полям
 
             Vector2DClass v1 = new Vector2DClass();
         }
@@ -29,10 +29,34 @@ namespace ConsoleTest
 
         }
 
+        public double Length => Math.Sqrt(_X * _X + _Y * _Y); //Длина вектора
+       
         public Vector2DClass(double _X,double _y)
         {
             this._X = _X;
             this._Y = _Y;
+        }
+    
+        public static Vector2DClass operator +(Vector2DClass a, Vector2DClass b) // перегрузка операторов
+        {
+            return new Vector2DClass(a._X + b._X, a._Y + a._Y);
+        }
+        public static bool operator ==(Vector2DClass a, Vector2DClass b)
+        {
+            return a._X == b._X && a._Y == b._Y;
+        }
+        public static bool operator !=(Vector2DClass a, Vector2DClass b)
+        {
+            return !(a == b);
+        }
+        public static implicit /*implicit переопределяем не явный оператор типов*/ operator double/* в double   из*/ (Vector2DClass v) // Оператор поведения
+        {
+            return v.Length;
+        }
+
+        public static explicit /*explicit явное преобразование оператор типов*/ operator int/* в int   из*/ (Vector2DClass v) // Оператор поведения
+        {
+            return (int)v.Length;
         }
     }
 }
