290d6c810ff30c7afe99533ca0cf0a4669110dd0
TouchPad <olsher258@mail.ru>
2021-01-17 13:26:22 +0300

    Пуля и возможность проверки для пули

---
+++ AsteroidGame/ICollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AsteroidGame
+{
+  internal  interface ICollision
+    {
+        Rectangle Rect { get; }
+
+        bool CheckCollision(ICollision obj);
+    }
+}
--- AsteroidGame/VisualObject/Asteroid.cs
+++ AsteroidGame/VisualObject/Asteroid.cs
@@ -5,13 +5,21 @@ using System.Text;
 
 namespace AsteroidGame.VisualObject
 {
-    internal class Asteroid : ImageObject
+    internal class Asteroid : ImageObject, ICollision
     {
         private static readonly Image _Image = Image.FromFile("image/ast.png");
+
+        public int Power { get; set; }
+
+        public Rectangle Rect => new Rectangle(_Position, _Size); // астеройд возвращает облост которую он занимает
+
         public Asteroid(Point Position, Point Direction, int size) 
             : base(Position, Direction, new Size(size,size),_Image)
         {
 
         }
+
+        public bool CheckCollision(ICollision obj) => Rect.IntersectsWith(obj.Rect); // выполнения проверки н астолкновения
+       
     }
 }
